55cb81cf99da0c8bef0082864c49f9b191c004c5
Roman <bashkatovr@mail.ru>
2022-12-12 15:20:14 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    printLeapOrNorLeapYear(2029);
+    }
+    public static boolean isLeapYear(int year) {
+        return year % 4==0 && year%100 !=0 || year%400==0;
+    }
+    public static void leapOrNorLeapYear (int year) {
+        if (isLeapYear(year)) {
+            System.out.println(year + " год високосный");
+            return;
+        }
+        System.out.println(year + " год невисокосный");
     }
 }
