aeb3ec760b0ccb0e06c505db67712b7fa8c1c4ae
Roman <bashkatovr@mail.ru>
2022-12-12 15:50:46 +0300

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.time.LocalDate;
+
 public class Main {
+    public final static int CURRENTYear = LocalDate.now().getYear();
     public static void main(String[] args) {
     printLeapOrNorLeapYear(2029);
     }
@@ -11,5 +14,21 @@ public class Main {
             return;
         }
         System.out.println(year + " год невисокосный");
+    }
+    public static boolean IsNowYear (int year) {
+        return year==CURRENTYear;
+    }
+    public static String getVersionOS (int versionOS) {
+        if (versionOS == 0) {
+            return "IOS";
+        }
+            return "Andriod";
+        }
+    public static void printVersionOS (int year, int versionOS) {
+        if (IsNowYear(year)) {
+            System.out.println(" Установите полную версию для вашего "+getVersionOS (versionOS));
+        } else {
+            System.out.println(" Установите облегченную версию для вашего "+getVersionOS (versionOS));
+        }
     }
 }
